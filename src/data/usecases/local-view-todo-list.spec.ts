9ab41807c673f9b09691befa73457553fdd73a9f
Junior <vilmarmjrr@gmail.com>
2021-01-11 22:32:12 -0300

    refactor: create separate function to make sut

--- src/data/usecases/local-view-todo-list.spec.ts
+++ src/data/usecases/local-view-todo-list.spec.ts
@@ -1,10 +1,24 @@
 import { GetStorageSpy } from '../test/mock-cache';
 import { LocalViewTodoList } from './local-view-todo-list';
 
+type SutTypes = {
+  sut: LocalViewTodoList;
+  getStorageSpy: GetStorageSpy;
+};
+
+const makeSut = (): SutTypes => {
+  const getStorageSpy = new GetStorageSpy();
+  const sut = new LocalViewTodoList(getStorageSpy);
+
+  return {
+    sut,
+    getStorageSpy
+  };
+};
+
 describe('LocalViewTodoList', () => {
   test('Should call GetStorage with correct key', async () => {
-    const getStorageSpy = new GetStorageSpy();
-    const sut = new LocalViewTodoList(getStorageSpy);
+    const { sut, getStorageSpy } = makeSut();
 
     await sut.filter();
     expect(getStorageSpy.key).toBe('todos');
