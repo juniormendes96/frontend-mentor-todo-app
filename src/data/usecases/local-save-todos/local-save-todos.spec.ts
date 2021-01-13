6b169cfb239034ba65d6b7282c6f0973cd132ef2
Junior <vilmarmjrr@gmail.com>
2021-01-12 23:20:57 -0300

    test: ensure LocalSaveTodos calls get and set storage with correct key

--- src/data/usecases/local-save-todos/local-save-todos.spec.ts
+++ src/data/usecases/local-save-todos/local-save-todos.spec.ts
@@ -4,6 +4,7 @@ import { LocalSaveTodos } from '@/data/usecases';
 
 type SutTypes = {
   sut: LocalSaveTodos;
+  getStorageSpy: GetStorageSpy;
   setStorageSpy: SetStorageSpy;
 };
 
@@ -14,17 +15,19 @@ const makeSut = (): SutTypes => {
 
   return {
     sut,
+    getStorageSpy,
     setStorageSpy
   };
 };
 
 describe('LocalSaveTodos', () => {
   describe('create', () => {
-    test('Should call SetStorage with correct key', async () => {
-      const { sut, setStorageSpy } = makeSut();
+    test('Should call SetStorage and GetStorage with correct key', async () => {
+      const { sut, getStorageSpy, setStorageSpy } = makeSut();
 
       await sut.create(mockCreateTodoParams());
 
+      expect(getStorageSpy.key).toBe('todos');
       expect(setStorageSpy.key).toBe('todos');
     });
   });
