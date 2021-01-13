9e42ca415a2940a54a25f66ac660f951141f1e0d
Junior <vilmarmjrr@gmail.com>
2021-01-12 22:18:30 -0300

    feat: ensure LocalViewTodoList returns only active (non-completed) todos

--- src/data/usecases/local-view-todo-list/local-view-todo-list.ts
+++ src/data/usecases/local-view-todo-list/local-view-todo-list.ts
@@ -1,11 +1,16 @@
 import { Todo } from '@/domain/models';
-import { ViewTodoList, ViewTodoListFilters } from '@/domain/usecases';
+import { ViewTodoList, ViewTodoListFilters, ViewTodoListStatus } from '@/domain/usecases';
 import { GetStorage } from '@/data/protocols/cache';
 
 export class LocalViewTodoList implements ViewTodoList {
   constructor(private readonly getStorage: GetStorage) {}
 
   async filter(filters?: ViewTodoListFilters): Promise<Todo[]> {
+    const todos: Todo[] = this.getStorage.get('todos');
+
+    if (filters?.status === ViewTodoListStatus.ACTIVE) {
+      return todos.filter(todo => !todo.completed);
+    }
     return this.getStorage.get('todos');
   }
 }
