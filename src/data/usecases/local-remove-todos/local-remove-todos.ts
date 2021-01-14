e54c28c00ab0a0574ffeff0b661ce84d2f2b77b0
Junior <vilmarmjrr@gmail.com>
2021-01-14 18:44:14 -0300

    feat: ensure LocalRemoveTodos.remove removes todo by id

--- src/data/usecases/local-remove-todos/local-remove-todos.ts
+++ src/data/usecases/local-remove-todos/local-remove-todos.ts
@@ -1,11 +1,13 @@
 import { GetStorage, SetStorage } from '@/data/protocols/cache';
+import { Todo } from '@/domain/models';
 import { RemoveTodos } from '@/domain/usecases';
 
 export class LocalRemoveTodos implements RemoveTodos {
   constructor(private readonly getStorage: GetStorage, private readonly setStorage: SetStorage) {}
 
   remove(id: number): void {
-    this.getStorage.get('todos');
-    this.setStorage.set('todos', null);
+    const todos: Todo[] = this.getStorage.get('todos') || [];
+    const filteredTodos = todos.filter(todo => todo.id !== id);
+    this.setStorage.set('todos', filteredTodos);
   }
 }
