061d23da710301a3a0019d6efa48d2ad3849a0c9
Junior <vilmarmjrr@gmail.com>
2021-01-15 21:08:03 -0300

    test: ensure LocalStorageAdapter calls localStorage.setItem with correct values

---
+++ src/infra/cache/local-storage-adapter/local-storage-adapter.spec.ts
@@ -0,0 +1,21 @@
+import faker from 'faker';
+import 'jest-localstorage-mock';
+import { LocalStorageAdapter } from '@/infra/cache';
+
+const makeSut = (): LocalStorageAdapter => new LocalStorageAdapter();
+
+describe('LocalStorageAdapter', () => {
+  beforeEach(() => {
+    localStorage.clear();
+  });
+
+  test('Should call localStorage.setItem with correct values', () => {
+    const sut = makeSut();
+    const key = faker.database.column();
+    const value = faker.random.arrayElements();
+
+    sut.set(key, value);
+
+    expect(localStorage.setItem).toHaveBeenCalledWith(key, JSON.stringify(value));
+  });
+});
