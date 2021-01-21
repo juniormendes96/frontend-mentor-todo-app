91c40a364c65ce0955c018b6f841e31ffe314e40
Junior <vilmarmjrr@gmail.com>
2021-01-21 19:26:47 -0300

    chore: remove declaration files from coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}', '!<rootDir>/src/**/index.ts', '!<rootDir>/src/main/**/*'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}', '!<rootDir>/src/**/index.ts', '!<rootDir>/src/main/**/*', '!**/*.d.ts'],
   coverageDirectory: 'coverage',
   setupFilesAfterEnv: ['<rootDir>/src/main/config/jest-setup.ts'],
   testPathIgnorePatterns: ['<rootDir>/node_modules/'],
