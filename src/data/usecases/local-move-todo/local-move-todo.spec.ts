71cf38b022803a807f04355e9e3204041976f4c3
Junior <vilmarmjrr@gmail.com>
2021-01-26 19:02:42 -0300

    test: ensure LocalMoveTodo moves todo to the new position

--- src/data/usecases/local-move-todo/local-move-todo.spec.ts
+++ src/data/usecases/local-move-todo/local-move-todo.spec.ts
@@ -2,6 +2,7 @@ import faker from 'faker';
 
 import { GetStorageSpy, SetStorageSpy } from '@/data/test';
 import { LocalMoveTodo } from '@/data/usecases';
+import { mockTodos } from '@/domain/test';
 
 type SutTypes = {
   sut: LocalMoveTodo;
@@ -10,8 +11,10 @@ type SutTypes = {
 };
 
 const makeSut = (): SutTypes => {
-  const getStorageSpy = new GetStorageSpy();
   const setStorageSpy = new SetStorageSpy();
+  const getStorageSpy = new GetStorageSpy();
+  getStorageSpy.value = mockTodos();
+
   const sut = new LocalMoveTodo(getStorageSpy, setStorageSpy);
 
   return {
@@ -29,5 +32,15 @@ describe('LocalMoveTodo', () => {
 
     expect(getStorageSpy.key).toBe('todos');
     expect(setStorageSpy.key).toBe('todos');
+  });
+
+  test('Should move todo to the new position', async () => {
+    const { sut, setStorageSpy } = makeSut();
+
+    const reorderedTodos = await sut.invoke(1, 1);
+
+    expect(reorderedTodos[0].id).toBe(2);
+    expect(reorderedTodos[1].id).toBe(1);
+    expect(setStorageSpy.value).toEqual(reorderedTodos);
   });
 });
