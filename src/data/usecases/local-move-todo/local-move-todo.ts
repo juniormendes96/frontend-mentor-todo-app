ab4f87f3e08de1b40b41d414cde2afa63f128b64
Junior <vilmarmjrr@gmail.com>
2021-01-26 19:02:34 -0300

    feat: ensure LocalMoveTodo moves todo to the new position

--- src/data/usecases/local-move-todo/local-move-todo.ts
+++ src/data/usecases/local-move-todo/local-move-todo.ts
@@ -6,8 +6,19 @@ export class LocalMoveTodo implements MoveTodo {
   constructor(private readonly getStorage: GetStorage, private readonly setStorage: SetStorage) {}
 
   invoke(id: number, newIndex: number): Promise<Todo[]> {
-    this.getStorage.get('todos');
-    this.setStorage.set('todos', []);
-    return Promise.resolve([]);
+    const todos = this.getTodosFromStorage();
+    const todo = todos.find(todo => todo.id === id);
+    const oldPositionTodo = todos[newIndex];
+    const oldIndex = todos.indexOf(todo);
+
+    todos[newIndex] = todo;
+    todos[oldIndex] = oldPositionTodo;
+
+    this.setStorage.set('todos', todos);
+    return Promise.resolve(todos);
+  }
+
+  private getTodosFromStorage(): Todo[] {
+    return this.getStorage.get('todos') || [];
   }
 }
